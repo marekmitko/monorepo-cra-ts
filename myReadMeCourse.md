8ca5072d036cbb4ea41b79ef08f4ee8596b2e2db
marekmitko <mitko.email@gmail.com>
2022-08-19 02:03:11 +0200

    first commit

---
+++ myReadMeCourse.md
@@ -0,0 +1,9 @@
+ 
+ 
+ remove in lerna.json 
+  "$schema": "node_modules/lerna/schemas/lerna-schema.json",
+    "useNx": true,
+
+
+        "start": "tsc && node dist/server.js "
+        gdzie tsc - odpowiada za konwersje server.ts -> server.js 
---
+++ packages/simple-react-app/src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { QueryPayload } from 'simple-node-server/server';
+import DarkMode from './react-dark-mode/src/DarkMode';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DarkMode />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button
+            onClick={() => {
+            fetch("http://localhost:3001/data", {})
+                .then((response) => response.json())
+                .then((data: QueryPayload) => console.log(data.foo));
+            }}
+        >
+            GET SOME DATA
+        </button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
